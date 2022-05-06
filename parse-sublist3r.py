d2454ddf0d089cffa81bb7c4f5cf4610bc2597e8
Lucas Pagliuca Diniz <57265360+LucasPDiniz@users.noreply.github.com>
2022-05-06 00:00:29 -0300

    Create parse-sublist3r.py

---
+++ parse-sublist3r.py
@@ -0,0 +1,17 @@
+import sys
+import string
+import json
+
+#Sublist3r --> github.com/aboul3la/Sublist3r
+dic_sublist3r = {}
+
+def parse():
+    with open('/home/kali/Downloads/Sublist3r/strings.txt') as file:
+        for line in file:
+            dic_sublist3r['subdomain'] = line.rstrip('\n')
+            print(dic_sublist3r)
+def main():
+    parse()
+
+if __name__== '__main__':
+    main()
